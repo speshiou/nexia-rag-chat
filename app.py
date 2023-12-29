22520f285328db6d550de8a244a8b70be2985432
pshaung <speshiou@gmail.com>
2023-12-29 20:32:01 +0800

    Replace annotations of citations

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if "thread_id" not in st.session_state:
     st.session_state["thread_id"] = thread.id
     db.upsert_chat(thread.id)
 
-st.title("Thread #{}".format(st.session_state["thread_id"]))
+st.title("#{}".format(st.session_state["thread_id"]))
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
@@ -56,17 +56,37 @@ if prompt := st.chat_input("What is up?"):
                     run_id=run.id
                 )
                 message_placeholder.markdown(run.status)
-                print(run)
 
         messages = client.beta.threads.messages.list(
-            thread_id=st.session_state["thread_id"]
+            thread_id=st.session_state["thread_id"],
         )
 
         for message in messages:
-            print(message)
-            content = message.content[0].text.value
-            st.session_state.messages.append({"role": message.role, "content": content})
-            message_placeholder.markdown(content)
+            # Extract the message content
+            message_content = message.content[0].text
+            annotations = message_content.annotations
+            citations = []
 
-            db.push_chat_history(st.session_state["thread_id"], prompt, content)
+            # Iterate over the annotations and add footnotes
+            for index, annotation in enumerate(annotations):
+                # Replace the text with a footnote
+                message_content.value = message_content.value.replace(annotation.text, f' [{index}]')
+
+                # Gather citations based on annotation attributes
+                if (file_citation := getattr(annotation, 'file_citation', None)):
+                    cited_file = client.files.retrieve(file_citation.file_id)
+                    citations.append(f'[{index}] {file_citation.quote} from {cited_file.filename}')
+                elif (file_path := getattr(annotation, 'file_path', None)):
+                    cited_file = client.files.retrieve(file_path.file_id)
+                    citations.append(f'[{index}] Click <here> to download {cited_file.filename}')
+                    # Note: File download functionality not implemented above for brevity
+
+            # Add footnotes to the end of the message before displaying to user
+            # message_content.value += '\n\n' + '\n'.join(citations)
+            st.session_state.messages.append({"role": message.role, "content": message_content.value})
+            message_placeholder.markdown(message_content.value)
+
+            db.push_chat_history(st.session_state["thread_id"], prompt, message_content.value)
+
+            # retrieve only the newest message
             break
